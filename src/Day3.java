eae439ac5e7e57ba480f8df8ac5defecf3cd8286
1909606 <1909606@01-18114.pusd.dom>
2024-12-11 13:49:37 -0800

    day 3 class attempt

--- src/Day3.java
+++ src/Day3.java
@@ -3,11 +3,40 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day3 {
 	
 	
+	public static int multiply(String mul) {
+		int res = 0;
+		
+		
+		res = Integer.parseInt(mul.substring(4, mul.indexOf(","))) * Integer.parseInt(mul.substring(mul.indexOf(","+1), mul.indexOf(")")));
+		return res;
+	}
 	
+	/*
+	 * Extract the next multiplication command from the given line
+	 * 
+	 * returns an empty string if none exist
+	 */
+	public static String extract(String line) {
+		
+		if(line.indexOf("mul(") == -1) {
+			return ""; // not valid command found in line
+		} else {
+			int start = line.indexOf("mul(");
+			int end = line.indexOf(")");
+			
+			//STOP! if there is no closing ")" then this is not the one we want :(
+			
+			
+			return line.substring(start, end);
+		}
+		
+	}
 	
 	
 	public static void main(String[] args) {
@@ -27,9 +56,26 @@ public class Day3 {
 				
 				while(scan.hasNext())
 				{	
-					String line = scan.nextLine();
-					//split all of the calls of the mul(X,Y) by ")"
-					String[] elements = line.split(")");
+					
+					
+					String line = scan.next();
+					
+					String regex = "mul\\((\\d+),(\\d+\\)";
+					
+					Pattern pattern = Pattern.compile(regex);
+					Matcher matcher = pattern.matcher(line);
+					
+					ArrayList<String> list = new ArrayList<String>(); 
+					
+					while(matcher.find()) {
+						list.add(matcher.group());					
+					}
+					
+					System.out.println("Valid 'mul' calls:");
+					for(String call : list) {
+						System.out.println(call);
+					}
+					
 					
 					
 					
@@ -37,15 +83,14 @@ public class Day3 {
 					
 				}
 			
-				
+				scan.close();
 				
 					
 				System.out.println("TOTAL SUM " + sum);
 					
 					//always close a scanner
 					
-					scan.close();
-					
+				
 				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
